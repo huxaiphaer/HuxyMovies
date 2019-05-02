8dbd6ac05fa22b48bdfba45a3a75a8bf470c751b
huxaiphaer <l654321123>
2019-05-02 14:24:41 +0300

    - (chore) add unit tests   * Testing Room database.

---
+++ app/src/main/java/com/movieapp/huxymovies/data/local/ResultsDao.kt
@@ -0,0 +1,27 @@
+package com.movieapp.huxymovies.data.local
+
+import android.arch.lifecycle.LiveData
+import android.arch.paging.DataSource
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import com.movieapp.huxymovies.model.Result
+
+@Dao
+interface ResultsDao {
+
+    @Query("SELECT * FROM Results")
+    fun getAllMovies(): LiveData<List<Result>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllMovies(moviesList: List<Result>)
+
+    @Query("DELETE FROM Results")
+    fun deleteAllMovies()
+
+    @Query("SELECT * FROM Results")
+    fun selectPaged(): DataSource.Factory<Int, Result>
+
+}
+
